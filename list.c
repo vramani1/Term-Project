ba1cd6b748c3417b00de38c0d8bd2e3bab826ab6
vramani1 <vramani2@scu.edu>
2026-05-31 19:46:36 -0700

    struct definitions

--- list.c
+++ list.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "list.h" 
 
+#define INITIAL_CAPACITY 10
+
+typedef struct node {
+    void **data;        
+    int length;         
+    int count;         
+    int first;          
+    struct node *next;  
+    struct node *prev;  
+} NODE;
+
+struct list {
+    NODE *head;
+    NODE *tail;
+    int count;
+};
